85f94f4be02a133fbe12663ea9166f49f86e591f
Alexis <alexis3222@hotmail.com>
2025-12-27 21:23:09 -0500

    feat(header): add Socio Comercial CTA button

--- components/SiteHeader.tsx
+++ components/SiteHeader.tsx
@@ -3,6 +3,7 @@
 
 import { usePathname } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 import { FranjaNaranjaRotativa } from "./FranjaNaranjaRotativa";
 
 export function SiteHeader() {
@@ -17,15 +18,39 @@ export function SiteHeader() {
             {/* Banda negra SOLO en la página principal */}
             {esHome && (
                 <div className="w-full bg-black/35">
-                    <div className="mx-auto flex max-w-6xl items-center px-0 py-0">
+                    <div className="mx-auto flex max-w-6xl items-center justify-between px-4 py-2">
+
+                        {/* LOGO */}
                         <Image
                             src="/logo-bikers.svg"
-                            alt="Bikers Motors"
+                            alt="Casa Bikers"
                             width={300}
                             height={80}
                             className="w-[260px] h-auto md:w-[320px]"
                             priority
                         />
+
+                        {/* BOTÓN SOCIO COMERCIAL */}
+                        <Link
+                            href="/socio-comercial"
+                            className="
+                rounded-full
+                border-2 border-[#FF7F00]
+                px-4 py-2
+                text-xs md:text-sm
+                font-extrabold
+                uppercase
+                tracking-[0.12em]
+                text-black
+                bg-transparent
+                transition-all
+                duration-200
+                hover:bg-[#FF7F00]
+                hover:text-white
+              "
+                        >
+                            Socio comercial
+                        </Link>
                     </div>
                 </div>
             )}
